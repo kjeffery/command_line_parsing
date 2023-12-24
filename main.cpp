6496d7cce7faab0b247915ad40211cb5cc3ff10d
Keith Jeffery <krjeffery@gmail.com>
2023-12-24 13:03:01 -0800

    Refactor Command Line Argument parsing
    Significantly refactored the argument parsing mechanism in main.cpp. Command line parsing was migrated from previously polymorphic ArgumentBase, ValueArgument, and PositionalArguments classes into a generalized CommandLineParser class and sub-structures. This new design increases modularity and improves the readability and maintainability of the code.

--- main.cpp
+++ main.cpp
@@ -1,36 +1,20 @@
 #include <iostream>
+#include <string_view>
+#include <thread>
 #include <unordered_map>
 
 template <typename T>
 struct Argument
 {
-    std::string_view short_name;
-    std::string_view long_name;
-    std::string_view description;
-    T                value;
+    std::string_view short_name{};
+    std::string_view long_name{};
+    std::string_view description{};
+    T                value{};
 };
 
-class ArgumentBase
+class SwitchArgument
 {
 public:
-    virtual ~ArgumentBase() = default;
-
-    [[nodiscard]] virtual ArgumentBase* clone() const = 0;
-
-    // template <typename T>
-    // void add(std::string_view scommand, std::string_view lcommand, std::string_view description, )
-
-private:
-};
-
-class SwitchArgument : public ArgumentBase
-{
-public:
-    [[nodiscard]] SwitchArgument* clone() const override
-    {
-        return new SwitchArgument(*this);
-    }
-
     [[nodiscard]] bool get() const noexcept
     {
         return m_value;
@@ -45,48 +29,79 @@ private:
     bool m_value{ false };
 };
 
-template <typename T>
-class ValueArgument : public ArgumentBase
+template <typename T, std::size_t count = 1>
+class ValueArgument
 {
 public:
     explicit ValueArgument(Argument<T> in);
 
-    [[nodiscard]] ValueArgument* clone() const override
+    [[nodiscard]] const T& get(std::size_t idx = 0) const noexcept
     {
-        return new ValueArgument(*this);
+        return m_data[idx];
     }
 
-    [[nodiscard]] const T& get() const;
+private:
+    Argument<T>
+    std::array<T, count> m_data;
 };
 
 template <typename T>
-class PositionalArguments : public ArgumentBase
+class PositionalArguments
 {
 public:
-    [[nodiscard]] PositionalArguments* clone() const override
-    {
-        return new PositionalArguments(*this);
-    }
-
     [[nodiscard]] std::size_t size() const noexcept;
 
     auto begin() const;
     auto end() const;
     auto cbegin() const;
     auto cend() const;
+
+private:
+    std::vector<T> m_data;
 };
 
 class CommandLineParser
 {
+    struct ArgumentBase
+    {
+        virtual      ~ArgumentBase() = default;
+        virtual void read(std::istream& ins) = 0;
+
+        //[[nodiscard]] virtual ArgumentBase* clone() const = 0;
+    };
+
+    template <typename T>
+    struct TypedArgument : public ArgumentBase
+    {
+        std::string_view short_name;
+        std::string_view long_name;
+        std::string_view description;
+        T                value;
+
+        //[[nodiscard]] virtual TypedArgument* clone() const = 0;
+    };
+
 public:
-    void parse(const int argc, const char* argv[]);
+    void parse(const int argc, const char* argv[])
+    {
+        for (int i = 1; i < argc; ++i) {
+            std::string_view s(argv[i]);
+            if (s.starts_with("--")) {
+            } else if (s.starts_with('-')) {
+            }
+        }
+    }
 
     void print_help();
 
-    void add(const ArgumentBase& argument)
+    template <typename T>
+    void add(const T& argument)
     {
-        // Clone
-        // Add to vector
+        if ()
+            // Create TypedArgument in unique_ptr
+            // Copy values
+
+
     }
 
     // template <typename T>
@@ -103,13 +118,30 @@ private:
 int main(int argc, const char* argv[])
 {
     try {
+        const auto default_threads = std::thread::hardware_concurrency();
+
+        CommandLineParser parser;
+
         const Argument<std::string> name_args = {
             .short_name = "n", .long_name = "name", .description = "User name", .value = "Marcus"
         };
-        ValueArgument name{name_args};
+        ValueArgument name{ name_args };
 
-        //ValueArgument<std::string> name{name_args};
-        const auto x = name.clone();
+        const Argument<std::size_t> thread_args = {
+            .long_name = "threads", .description = "Number of threads", .value = default_threads
+        };
+        ValueArgument               threads{ thread_args };
+
+        const Argument<std::size_t, 2> resolution_args = {
+            .short_name = "r", .long_name = "resolution", .description = "Number of threads", .value = default_threads
+        };
+
+        parser.add(name);
+        parser.add(threads);
+
+        parser.parse(argc, argv);
+
+        std::println(std::cout, "Got name: {}", name.get());
     } catch (const std::exception& e) {
     }
 }
