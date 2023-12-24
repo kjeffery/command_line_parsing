181434bd99718890c75440de17f63fb7f2306830
Keith Jeffery <krjeffery@gmail.com>
2023-12-24 11:19:06 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <unordered_map>
+
+template <typename T>
+struct Argument
+{
+    std::string_view short_name;
+    std::string_view long_name;
+    std::string_view description;
+    T value;
+};
+
+class ArgumentBase
+{
+public:
+    virtual ~ArgumentBase() = default;
+
+    [[nodiscard]] virtual ArgumentBase* clone() const = 0;
+
+    // template <typename T>
+    // void add(std::string_view scommand, std::string_view lcommand, std::string_view description, )
+
+private:
+};
+
+class SwitchArgument : public ArgumentBase
+{
+public:
+    [[nodiscard]] SwitchArgument* clone() const override
+    {
+        return new SwitchArgument(*this);
+    }
+
+    [[nodiscard]] bool get() const noexcept
+    {
+        return m_value;
+    }
+
+    explicit operator bool() const noexcept
+    {
+        return m_value;
+    }
+
+private:
+    bool m_value{ false };
+};
+
+template <typename T>
+class ValueArgument : public ArgumentBase
+{
+public:
+    [[nodiscard]] ValueArgument* clone() const override
+    {
+        return new ValueArgument(*this);
+    }
+
+    [[nodiscard]] const T& get() const;
+};
+
+template <typename T>
+class PositionalArguments : public ArgumentBase
+{
+public:
+    [[nodiscard]] PositionalArguments* clone() const override
+    {
+        return new PositionalArguments(*this);
+    }
+
+    [[nodiscard]] std::size_t size() const noexcept;
+
+    auto begin() const;
+    auto end() const;
+    auto cbegin() const;
+    auto cend() const;
+};
+
+class CommandLineParser
+{
+public:
+    void parse(const int argc, const char* argv[]);
+
+    void print_help();
+
+    void add(const ArgumentBase& argument)
+    {
+        // Clone
+        // Add to vector
+    }
+
+    // template <typename T>
+    // void add(std::string_view scommand, std::string_view lcommand, std::string_view description, )
+
+private:
+    std::unordered_map<std::string_view, std::string_view>              m_short_to_long;
+    std::unordered_map<std::string_view, std::unique_ptr<ArgumentBase>> m_long_to_arg;
+};
+
+int main(int argc, const char* argv[])
+{
+    try {
+
+        ValueArgument<std::string> name;
+        const auto                 x = name.clone();
+    } catch (const std::exception& e) {
+    }
+}
